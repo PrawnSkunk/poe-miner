150b84e0f696056f727cccd21420c25f874795b0
Max Proske <mhcproske@gmail.com>
2017-07-01 12:51:03 -0700

    First Installation

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 sudo apt-get update -y 
 sudo apt-get install git screen automake build-essential libcurl4-openssl-dev libgmp-dev autotools-dev -y 
 git clone https://github.com/noncepool/m7magi-cpuminer-v2 
+sudo chmod 755 mine.sh
+mv mine.sh m7magi-cpuminer-v2/mine.sh
 cd m7magi-cpuminer-v2
 sudo ./autogen.sh
 CFLAGS="-O3" ./configure
 make
 sudo cp minerd /usr/local/bin
-sudo ~/mine.sh
+sudo ./mine.sh
